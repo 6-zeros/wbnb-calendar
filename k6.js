401a6c01e6a0f4075af5240ba58da4cc34b79622
George <georgezqian@gmail.com>
2019-01-16 19:25:47 -0800

    Complete stress test for booking service back end

---
+++ k6.js
@@ -0,0 +1,17 @@
+import http from "k6/http";
+
+export let options = {
+  vus: 100,
+  duration: "5m",
+  rps: 50
+};
+
+export default function() {
+  const randomRoomId = Math.floor(Math.random() * 10000000) + 1;
+  const url = `http://localhost:8080/api/rooms/${randomRoomId}`;
+  http.get(url);
+};
+
+
+
+
