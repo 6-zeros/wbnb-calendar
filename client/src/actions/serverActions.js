48aba39147045bc753ac4f19f28c8af6de74ad0f
George <georgezqian@gmail.com>
2019-01-17 16:04:10 -0800

    Successfully reconnect GET route to front end

--- client/src/actions/serverActions.js
+++ client/src/actions/serverActions.js
@@ -7,12 +7,30 @@ const getData = roomID => (
   (dispatch) => {
     axios.get(`/api/rooms/${roomID}`)
       .then(({ data }) => {
-        dispatch(setPrice(data.price));
-        dispatch(setStars(data.stars));
-        dispatch(setCleaningFee(data.cleaningFee));
-        dispatch(setBookedDates(data.bookedDates));
-        dispatch(setPrice(data.price));
-        dispatch(setServiceFee(data.serviceFee));
+        const price = data[0].price;
+        const stars = data[0].stars;
+        const cleaningFee = data[0].cleaning_fee;
+        const serviceFee = data[0].service_fee;
+        const bookedDates = data.map(obj => {
+          const guests = {
+            adults: obj.adults,
+            children: obj.children,
+            infants: obj.infants
+          };
+
+          return {
+            startDate: obj.start_date,
+            endDate: obj.end_date,
+            guests: guests
+          };
+        });
+
+        dispatch(setPrice(price));
+        dispatch(setStars(stars));
+        dispatch(setCleaningFee(cleaningFee));
+        dispatch(setBookedDates(bookedDates));
+        dispatch(setServiceFee(serviceFee));
+        // dispatch(setPrice(data.price));
       });
   }
 );
